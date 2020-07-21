55a71c6e6a7bea3c10fa2beebad28c29b33b5c2a
Hayley van Waas <hayleyavw@gmail.com>
2020-07-21 13:43:33 +0800

    Added starter files for project.

---
+++ she_codes_news/news/views.py
@@ -0,0 +1,16 @@
+from django.views import generic
+from .models import NewsStory
+
+
+class IndexView(generic.ListView):
+    template_name = 'news/index.html'
+
+    def get_queryset(self):
+        '''Return all news stories.'''
+        return NewsStory.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['all_stories'] = NewsStory.objects.all()
+        return context
